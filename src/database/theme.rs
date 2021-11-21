6927eef1ef3ae0872e63ebb86a42f8388b89176e
EAimTY <ea.imty@gmail.com>
2021-11-21 16:23:50 +0900

    new `Post` and `Page` API

--- src/database/theme.rs
+++ src/database/theme.rs
@@ -11,12 +11,18 @@ impl Theme {
 
     pub fn render(&self, data: Data) -> Response<Body> {
         match data {
-            Data::Post(post) => Response::new(Body::from(post.content.clone())),
-            Data::Page(page) => Response::new(Body::from(page.content.clone())),
+            Data::Post(post) => Response::new(Body::from(post.content().to_owned())),
+            Data::Page(page) => Response::new(Body::from(page.content().to_owned())),
             Data::List(list) => match list {
                 List::Index { data } => Response::new(Body::from(
                     data.into_iter()
-                        .map(|post| format!("{}\n{}\n\n", post.title.clone(), post.content.clone()))
+                        .map(|post| {
+                            format!(
+                                "{}\n{}\n\n",
+                                post.title().to_owned(),
+                                post.content().to_owned()
+                            )
+                        })
                         .collect::<String>(),
                 )),
                 _ => todo!(),
