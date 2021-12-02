a9b32080f7c3ca08f662fbdc156b8cc6d27caa55
EAimTY <ea.imty@gmail.com>
2021-12-02 19:51:23 +0900

    merge maps in `PathMap`

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -1,3 +1,5 @@
+use crate::Config;
+use anyhow::Result;
 use hyper::{Body, Request, Response};
 use once_cell::sync::OnceCell;
 use std::{collections::HashMap, convert::Infallible};
@@ -19,23 +21,28 @@ pub struct RouteTable {
 
 #[derive(Debug)]
 pub struct PathMap {
-    pub pages: HashMap<String, usize>,
-    pub posts: HashMap<String, usize>,
+    pub map: HashMap<String, DataType>,
 }
 
 impl PathMap {
-    pub fn new() -> Self {
-        Self {
-            pages: HashMap::new(),
-            posts: HashMap::new(),
-        }
+    pub fn init() -> Result<Self> {
+        let mut map = HashMap::new();
+
+        map.insert(
+            Config::read().url_patterns.index_url.to_owned(),
+            DataType::Index,
+        );
+
+        map.insert(
+            Config::read().url_patterns.update_url.to_owned(),
+            DataType::Update,
+        );
+
+        Ok(Self { map })
     }
 
     pub fn match_pattern(&self, path: &str) -> Option<DataType> {
-        self.pages.get(path).map_or_else(
-            || self.posts.get(path).map(|&id| DataType::Post { id }),
-            |&id| Some(DataType::Page { id }),
-        )
+        self.map.get(path).cloned()
     }
 }
 
@@ -53,13 +60,15 @@ impl PathTrie {
 }
 
 impl RouteTable {
-    pub fn init() {
+    pub fn init() -> Result<()> {
         ROUTE_TABLE
             .set(Self {
                 path_trie: PathTrie::new(),
-                path_map: RwLock::new(PathMap::new()),
+                path_map: RwLock::new(PathMap::init()?),
             })
             .unwrap();
+
+        Ok(())
     }
 
     pub async fn route(mut req: Request<Body>) -> Result<Response<Body>, Infallible> {
@@ -68,6 +77,16 @@ impl RouteTable {
         let path_map = route_table.path_map.read().await;
 
         match path_map.match_pattern(&req.uri().path()) {
+            Some(DataType::Index) => {
+                if let Some(res) = index::handle(&req).await {
+                    return Ok(res);
+                }
+            }
+            Some(DataType::Update) => {
+                if let Some(res) = update::handle(&mut req).await {
+                    return Ok(res);
+                }
+            }
             Some(DataType::Page { id: page_id }) => {
                 if let Some(res) = page::handle(&req, page_id).await {
                     return Ok(res);
@@ -82,35 +101,43 @@ impl RouteTable {
         }
 
         match route_table.path_trie.match_pattern(&req.uri().path()) {
-            Some(DataType::Index) => {
-                if let Some(res) = index::handle(&req).await {
-                    return Ok(res);
-                }
-            }
-            Some(DataType::Update) => {
-                if let Some(res) = update::handle(&mut req).await {
-                    return Ok(res);
-                }
-            }
             _ => {}
         }
 
         Ok(not_found::handle(&req).await)
     }
 
+    pub async fn clear_path_map() -> Result<()> {
+        let route_table = ROUTE_TABLE.get().unwrap();
+        let mut path_map = route_table.path_map.write().await;
+
+        *path_map = PathMap::init()?;
+
+        Ok(())
+    }
+
     pub async fn update_page_map(page_map: HashMap<String, usize>) {
         let route_table = ROUTE_TABLE.get().unwrap();
         let mut path_map = route_table.path_map.write().await;
-        path_map.pages = page_map;
+        path_map.map.extend(
+            page_map
+                .into_iter()
+                .map(|(path, id)| (path, DataType::Page { id })),
+        );
     }
 
     pub async fn update_post_map(post_map: HashMap<String, usize>) {
         let route_table = ROUTE_TABLE.get().unwrap();
         let mut path_map = route_table.path_map.write().await;
-        path_map.posts = post_map;
+        path_map.map.extend(
+            post_map
+                .into_iter()
+                .map(|(path, id)| (path, DataType::Post { id })),
+        );
     }
 }
 
+#[derive(Clone, Debug)]
 pub enum DataType {
     Post { id: usize },
     Page { id: usize },
