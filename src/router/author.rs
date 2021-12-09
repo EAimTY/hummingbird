0bf77f65b89fba840c7000f3dd8e0469be8afd0c
EAimTY <ea.imty@gmail.com>
2021-12-09 18:28:55 +0900

    some tiny optimizations

--- src/router/author.rs
+++ src/router/author.rs
@@ -5,12 +5,11 @@ pub async fn handle(req: &Request<Body>, author: &str) -> Option<Response<Body>>
     if req.method() == Method::GET {
         let db = DatabaseManager::read().await;
 
-        if let Some(post_ids) = db.authors.get_posts(author) {
-            let posts = db.posts.get_multi(post_ids);
+        let post_ids = db.authors.get_posts(author)?;
+        let posts = db.posts.get_multi(post_ids);
 
-            let res = db.theme.render_author(author, posts);
-            return Some(res);
-        }
+        let res = db.theme.render_author(author, posts);
+        return Some(res);
     }
     None
 }
