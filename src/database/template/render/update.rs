d06387612bb43715157da0056f2ddca2a3ef69c6
EAimTY <ea.imty@gmail.com>
2021-12-15 18:57:38 +0900

    add template for `not_found`

--- src/database/template/render/update.rs
+++ src/database/template/render/update.rs
@@ -5,7 +5,7 @@ use hyper::{Body, Response};
 impl Template {
     pub fn render_update(&self, result: DatabaseUpdateResult) -> Response<Body> {
         match result {
-            DatabaseUpdateResult::Success => Response::new(Body::from("ok")),
+            DatabaseUpdateResult::Success => Response::new(Body::from("success")),
             DatabaseUpdateResult::PermissionDenied => Response::builder()
                 .status(403)
                 .body(Body::from("permission denied"))
